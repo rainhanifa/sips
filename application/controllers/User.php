d679a4a35d1d2599ae6f8df9741996137b783628
rainhanifa <vj2daworld@gmail.com>
2017-08-07 14:15:24 +0700

    updating controller User

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -20,6 +20,34 @@ class User extends CI_Controller{
 
 	public function tambah()
 	{
+		// jika ada post
+		if(isset($_POST['submit'])){
+			// ambil value dari masing-masing input
+			$username	= $this->input->post("username");
+			$password	= $this->input->post("password");
+			$nama_lengkap= $this->input->post("nama_lengkap");
+			$role		= $this->input->post("user_role");
+
+			$user_data 	= array("username" => $username,
+								"password" => $password,
+								"nama_lengkap" => $nama_lengkap,
+								"role" => $role
+								);
+			//insert ke database
+			if($this->db->insert($this->table, $user_data)){
+				// kirim pesan berhasil
+				$this->session->set_flashdata('message','<div class="alert alert-success">Berhasil menambah user!</div>');
+
+				// redirect
+				redirect(base_url("user"));
+			}
+			else{
+				//kirim pesan error
+				$this->session->set_flashdata('message','<div class="alert alert-danger">Gagal menambah user!</div>');
+			}
+		}
+		//menampilkan form tambah
+
 		$this->load->view('template/header.php');
 		$this->load->view('user/tambah.php');
 		$this->load->view('template/footer.php');
