5f564a65bad7adad0f4d0e680c9dd0dc8165a70f
Puri Indah Rosita <puriindahrosita.ti8.poliwangi@gmail.com>
2017-08-07 11:32:01 +0700

    update tambah proyek

--- application/views/proyek/v_tambahProyek.php
+++ application/views/proyek/v_tambahProyek.php
@@ -11,20 +11,7 @@
                         </div>
                                         <!-- Nominal -->
                         <div class="body">
-                            <form class="form-horizontal">
-                                <div class="row clearfix">
-                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                                        <label for="email_address_2">Nama Member</label>
-                                    </div>
-                                    <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <input type="text" id="nama" class="form-control" placeholder="Masukkan Nama Member">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
+                            <form class="form-horizontal" <?php echo base_url('Proyek/tambah_aksi')?>" action="POST">
                                 <div class="row clearfix">
                                     <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
                                         <label for="email_address_2">Nama Proyek</label>
@@ -32,7 +19,20 @@
                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
                                         <div class="form-group">
                                             <div class="form-line">
-                                                <input type="text" id="proyek" class="form-control" placeholder="Masukkan Nama Proyek">
+                                                <input type="text" id="nama_proyek" class="form-control" placeholder="Masukkan Nama Proyek">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row clearfix">
+                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                        <label for="email_address_2">Kode Proyek</label>
+                                    </div>
+                                    <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                        <div class="form-group">
+                                            <div class="form-line">
+                                                <input type="text" id="kode_proyek" class="form-control" placeholder="Masukkan Kode Proyek">
                                             </div>
                                         </div>
                                     </div>
