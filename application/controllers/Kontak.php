0a89caf83ee67b01936d14b5a596fa3a495e56b6
Yogi adam pratama <yogiadam.p.ti8.poliwangi@gmail.com>
2017-08-08 16:25:47 +0700

    gikk yogikk

--- application/controllers/Kontak.php
+++ application/controllers/Kontak.php
@@ -2,31 +2,49 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Kontak extends CI_Controller {
+	var $table = "contacts";
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
+		//query bisa menggunakan 
+		$data['list']	= $this->db->query("SELECT * FROM contacts")->result_array();
+		//atau menggunakan builder get
+		$data['list']	= $this->db->get("contacts")->result_array();
+
 		$this->load->view('template/header.php');
-		$this->load->view('kontak/v_kontak.php');
+		$this->load->view('kontak/v_kontak.php', $data);
 		$this->load->view('template/footer.php');
 	}
 
 	public function tambah()
 	{
+		// jika ada post
+		if(isset($_POST['submit'])){
+			// ambil value dari masing-masing input
+			$name	= $this->input->post("name");
+			$institution	= $this->input->post("institution");
+			$telp= $this->input->post("telp");
+			$email		= $this->input->post("email");
+
+			$contacts_data 	= array("name" => $name,
+								"institution" => $institution,
+								"telp" => $telp,
+								"email" => $email
+								);
+			//insert ke database
+			if($this->db->insert($this->table, $contacts_data)){
+				// kirim pesan berhasil
+				$this->session->set_flashdata('message','<div class="alert alert-success">Berhasil menambah kontak!</div>');
+
+				// redirect
+				redirect(base_url("kontak"));
+			}
+			else{
+				//kirim pesan error
+				$this->session->set_flashdata('message','<div class="alert alert-danger">Gagal menambah kontak!</div>');
+			}
+		}
+
 		$this->load->view('template/header.php');
 		$this->load->view('kontak/v_tambahkontak.php');
 		$this->load->view('template/footer.php');
--- application/views/kontak/v_tambahkontak.php
+++ application/views/kontak/v_tambahkontak.php
@@ -12,7 +12,12 @@
                         </div>
 
                         <div class="body">
-                            <form class="form-horizontal">
+                        <?php if($this->session->flashdata("message") != ""){
+                                echo $this->session->flashdata("message");
+                            }
+                            ?>
+
+                            <form method="post" action="<?php echo base_url("kontak/tambah")?>" class="form-horizontal">
                                 <div class="row clearfix">
                                     <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
                                         <label for="email_address_2">Nama</label>
@@ -20,7 +25,7 @@
                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
                                         <div class="form-group">
                                             <div class="form-line">
-                                                <input type="text" id="email_address_2" class="form-control">
+                                                <input type="text" name="name"class="form-control">
                                             </div>
                                         </div>
                                     </div>
@@ -34,7 +39,7 @@
                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
                                         <div class="form-group">
                                             <div class="form-line">
-                                                <input type="text" id="email_address_2" class="form-control">
+                                                <input type="text" name="institution" id="email_address_2" class="form-control">
                                             </div>
                                         </div>
                                     </div>
@@ -47,7 +52,7 @@
                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
                                         <div class="form-group">
                                             <div class="form-line">
-                                                <input type="text" id="email_address_2" class="form-control" >
+                                                <input type="text" name="telp" id="email_address_2" class="form-control" >
                                             </div>
                                         </div>
                                     </div>
@@ -60,21 +65,20 @@
                                     <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
                                         <div class="form-group">
                                             <div class="form-line">
-                                                <input type="email" id="password_2" class="form-control" >
+                                                <input type="email" name="email" id="password_2" class="form-control" >
                                             </div>
                                         </div>
                                     </div>
                                 </div>
 
-
-                                <center>
-                                    <div class="row">
-                                        <a href="<?php echo base_url('kontak')?>/index"><button type="button" class="btn btn-primary m-t-15 waves-effect">Simpan</button>
-                                        <a href="<?php echo base_url('kontak')?>/index"><button type="button" class="btn btn-danger m-t-15 waves-effect">Batal</button></a>
+                                <div class="text-center">
+                                <div class="body">
+                                    <div class="button-info">
+                                    <input type="submit" class="btn btn-primary waves-effect" value="Simpan" name="submit">
+                                    <a href="<?php echo base_url('kontak')?>/index"><button type="button" class="btn btn-danger waves-effect">Batal</button></a> 
                                     </div>
-                                </center>
-                                
-                                </div>
+                                </div>                                
+                               </div>
                             </form>
                         </div>
                     </div>
